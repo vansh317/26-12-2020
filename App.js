ad6122dcfbf88c49ff26abd03509fe5519761fa1
vansh317 <63545363+vansh317@users.noreply.github.com>
2020-12-26 18:56:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { createAppContainer,createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import TransactionScreen from './screens/BookTransactionScreen';
+import SearchScreen from './screens/SearchScreen';
+import Loginscreen from './screens/loginscreen';
+export default class App extends React.Component {
+  render(){
+    return (
+      
+        <AppContainer />
+      
+    );
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  Transaction: {screen: TransactionScreen},
+  Search: {screen: SearchScreen},
+},
+{
+  defaultNavigationOptions: ({navigation})=>({
+    tabBarIcon: ()=>{
+      const routeName = navigation.state.routeName;
+      console.log(routeName)
+      if(routeName === "Transaction"){
+        return(
+          <Image
+          source={require("./assets/book.png")}
+          style={{width:40, height:40}}
+        />
+        )
+        
+      }
+      else if(routeName === "Search"){
+        return(
+          <Image
+          source={require("./assets/searchingbook.png")}
+          style={{width:40, height:40}}
+        />)
+        
+      }
+    }
+  })
+}
+);
+const switchnavigate  = createSwitchNavigator({
+  login:{screen:Loginscreen},
+  TabNavigator:{screen:TabNavigator}
+})
+const AppContainer =  createAppContainer(switchnavigate);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
